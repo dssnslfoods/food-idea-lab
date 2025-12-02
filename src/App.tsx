4cf865fc1ae100eabb8d4a9f9764abec260af25a
DSSNSL <dssnslfoods@gmail.com>
2025-12-02 21:27:35 +0700

    Update App.tsx
    Signed-off-by: DSSNSL <dssnslfoods@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* ผมเติม basename ให้แล้วครับ */}
+      <BrowserRouter basename="/food-idea-lab">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
